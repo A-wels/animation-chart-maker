334489d448cc83307128925413537339e854b286
Alex <kontakt@a-wels.de>
2023-10-10 10:24:50 +0200

    update roadmap

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -8,27 +8,36 @@
 
 	<p>
 		This is a simple application to create animation charts. Simply set the width of the chart, add
-		boxes as you like and drag them around to create your chart. The code is available on <a href="https://github.com/A-wels/animation-chart-maker/"> Github </a>.
+		boxes as you like and drag them around to create your chart. The code is available on <a
+			href="https://github.com/A-wels/animation-chart-maker/"
+		>
+			Github
+		</a>.
 	</p>
 
 	<h2>Current features</h2>
 	<ul>
 		<li>Set chart width</li>
 		<li>Alternate number locations</li>
-		<li>Add and remove boxes</li>
+		<li>Add and remove frames</li>
 		<li>Select presets</li>
 		<ul>
 			<li>Equal distribution</li>
-		</ul>
-		<li>Save chart as image</li>
-	</ul>
-	<h2>Planned features</h2>
-	<ul>
-		<li>More presets</li>
-		<ul>
 			<li>SLO-IN</li>
 			<li>SLO-OUT</li>
 		</ul>
+		<li>Save chart as image</li>
+	</ul>
+
+	<h2>Planned features</h2>
+	<ul>
+		<li>Automatically mark keyframes</li>
+		<li>Manually mark breakdown frames with rightclick</li>
+		<li>More presets</li>
+		<ul>
+			<li>SLO-IN and SLO-OUT</li>
+		</ul>
+		<ul />
 		<li>Add image preview</li>
 		<li>Directly insert into image</li>
 		<li>MAYBE: Edit TVPP files</li>
